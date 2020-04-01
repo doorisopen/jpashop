50094152f4fa114a2f09db43d1af1f5bdf484efc
doorisopen <audauddl2@gmail.com>
2020-04-01 22:41:40 +0900

    Domain design

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // Enum 타입에 꼭 넣어줘야한다 (STRING 타입으로)
+    private DeliveryStatus status; // READY, COMP
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // 나는 연관관계 주인이 아니에요~
+    private List<Order> orders = new ArrayList<>();
+
+}
