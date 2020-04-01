82e3cfbe8eb175fe9c804d8875f4fae1c5c8c757
doorisopen <audauddl2@gmail.com>
2020-04-01 15:07:33 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository // Entity를 찾아주는 역할 DAO라고 생각하면된다.
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    // 커맨드와 쿼리를 분리해라!!
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
