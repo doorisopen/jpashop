7e610c3f2a3082785fa9e90bc3a669bbf58bf210
doorisopen <audauddl2@gmail.com>
2020-04-06 22:59:09 +0900

    Order Domain

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -24,4 +27,29 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
+
+    //==생성 메서드==//
+    public  static OrderItem createOrderItem(Item item, int orderPrice, int count) { // 할인 쿠폰 등 때문에 이렇게 넣어준다.
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() { // 제고 수량을 원복해준다.
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /**
+     * 주문상품 전체 가격 조회
+     * @return 주문상품 전체 가격
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
